e2a9487325fbd9997084a6a780aa2fa92735f888
zzlev <gpalektzja@gmail.com>
2023-03-20 23:27:49 +0900

    Add relationships.

---
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Category {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String name;
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> children = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name = "category_item",
+            joinColumns = @JoinColumn(name = "category_id"),
+            inverseJoinColumns = @JoinColumn(name = "item_id")
+    )
+    private List<Item> items = new ArrayList<>();
+}
